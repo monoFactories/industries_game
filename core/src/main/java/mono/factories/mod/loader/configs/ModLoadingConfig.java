4266ca8f3b501647f9b430b8583c77e2a3a2712f
monoFactories <msavranskij6@gmail.com>
2026-04-10 07:10:37 +0300

    add implements HasDependencies

--- core/src/main/java/mono/factories/mod/loader/configs/ModLoadingConfig.java
+++ core/src/main/java/mono/factories/mod/loader/configs/ModLoadingConfig.java
@@ -5,11 +5,13 @@ import io.github.g00fy2.versioncompare.Version;
 import it.unimi.dsi.fastutil.objects.Object2ObjectMap;
 import it.unimi.dsi.fastutil.objects.Object2ObjectMaps;
 import it.unimi.dsi.fastutil.objects.Object2ObjectOpenHashMap;
+import mono.factories.dependencies.HasDependency;
 import mono.factories.mod.loader.util.ModVersionRange;
+import mono.factories.registries.id.Identifier;
 
 import java.util.Map;
 
-public class ModLoadingConfig {
+public class ModLoadingConfig implements HasDependency {
     private static final Gson GSON;
 
     private final String id;
@@ -37,6 +39,22 @@ public class ModLoadingConfig {
                     return new ModLoadingConfig(id, version, Object2ObjectMaps.unmodifiable(dependencies));
                 })
                 .create();
+    }
+
+    @Override
+    public Identifier[] dependencies() {
+        Identifier[] depends = new Identifier[dependencies.size()];
+        int i = 0;
+        for (Map.Entry<String, ModVersionRange> entry : dependencies.entrySet()) {
+            depends[i] = new Identifier(entry.getKey());
+            i++;
+        }
+        return depends;
+    }
+
+    @Override
+    public Identifier id() {
+        return new Identifier(id);
     }
 }
 /*
