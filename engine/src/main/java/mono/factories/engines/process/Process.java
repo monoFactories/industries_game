795abbd5ff4b6642d733fc4b6d1eaa7e5871e466
monoFactories <msavranskij6@gmail.com>
2026-04-22 05:20:38 +0300

    rewrite with using CheckedHasDependency class

--- engine/src/main/java/mono/factories/engines/process/Process.java
+++ engine/src/main/java/mono/factories/engines/process/Process.java
@@ -1,27 +1,24 @@
 package mono.factories.engines.process;
 
+import mono.factories.dependencies.CheckedHasDependency;
 import mono.factories.dependencies.HasDependency;
 import mono.factories.engines.engine.Engine;
 import mono.factories.registries.id.Identifier;
 
 public abstract class Process implements HasDependency {
-    private final Identifier id;
-    private final Identifier[] dependencies;
+    private final CheckedHasDependency hasDependency;
 
     public Process(Identifier id, Identifier[] dependencies) {
         if (id == null) throw new NullPointerException("id is null");
-        this.id = id;
-        this.dependencies = dependencies == null ? new Identifier[0] : dependencies;
+        this.hasDependency = new CheckedHasDependency(dependencies, id);
     }
-
+    public abstract void run(Engine engine);
     @Override
     public final Identifier id() {
-        return id;
+        return hasDependency.id();
     }
-
-    public abstract void run(Engine engine);
-
+    @Override
     public final Identifier[] dependencies() {
-        return dependencies;
+        return hasDependency.dependencies();
     }
 }
