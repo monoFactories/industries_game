a3a00a581658ccd791bbee432d1b702c6a78f1d5
monoFactories <msavranskij6@gmail.com>
2026-04-28 04:31:24 +0300

    delete tags (bad code style)

--- engine/src/main/java/mono/factories/dependencies/HasTag.java
+++
@@ -1,9 +0,0 @@
-package mono.factories.dependencies;
-
-import mono.factories.registries.id.Identifiable;
-import mono.factories.registries.id.Identifier;
-
-public interface HasTag extends Identifiable {
-    Identifier[] dependencies();
-    Identifier[] tags();
-}
