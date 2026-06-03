356d16ea0def7c926c45fc08e0246ef80e6e3bec
monoFactories <msavranskij6@gmail.com>
2026-06-03 14:59:22 +0300

    added method getOrDefault(T t)

--- registries/src/main/java/mono/factories/registries/storage/DefaultHolder.java
+++ registries/src/main/java/mono/factories/registries/storage/DefaultHolder.java
@@ -12,7 +12,11 @@ public class DefaultHolder<T> {
     public T get() {
         return current == null ? constant : current;
     }
-
+    public T getOrDefault(T t) {
+        if (t == null)
+            return get();
+        return t;
+    }
     public void set(T t) {
         current = t;
     }
